968867dd7d4adc1002a54a4749aac5d556b8ae0d
MatteoRuffa <cortese_17@protonmail.com>
2024-03-15 14:54:31 +0100

    add insert bigSquare at click

--- js-script/script.js
+++ js-script/script.js
@@ -11,3 +11,16 @@ con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 
 con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
 con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
 */
+
+
+// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+{/* <div class="big-box"></div> */}
+
+const elButton = document.querySelector('.btn');
+
+elButton.addEventListener('click', function(){
+    const elBigSquare = document.createElement("div");
+    elBigSquare.classList = 'big-box';
+    console.log(elBigSquare);
+    document.querySelector('.container').appendChild(elBigSquare);
+});
